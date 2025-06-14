2e7afd3d6214942d8fef49aca987ebdf495f074c
Maria <mlcontrerast@ufpso.edu.co>
2025-06-14 07:55:06 -0500

    se pudo algo

--- src/TemperaturasSemanales.java
+++ src/TemperaturasSemanales.java
@@ -8,16 +8,16 @@ public class TemperaturasSemanales {
         mostrarMatriz(temperaturas);
 
         int altas = contarDiasCalurosos(temperaturas);
-        System.out.println("\nTemperaturas mayores a 30: " + altas);
+        System.out.println("\n Dias con temperaturas mayores a 30: " + altas);
 
-        double promedioBajas = promedioTemperaturasBajas(temperaturas);
+        double promedioBajas = promedioDiasFrescos(temperaturas);
         System.out.println("Promedio de temperaturas menores a 20: " + promedioBajas);
 
         System.out.println("\nTemperaturas críticas (mayores a 35):");
-        imprimirTemperaturasPeligrosas(temperaturas);
+        imprimirTemperaturasExtremas(temperaturas);
 
-        int[] pos = equipoMasCaliente(temperaturas);
-        System.out.println("\nEquipo más caliente está en [" + pos[0] + "][" + pos[1] + "] con " + temperaturas[pos[0]][pos[1]] + " grados");
+        int[] posicion = posicionTemperaturaMasAlta(temperaturas);
+        System.out.println("\nTemperatura mas alta está en la [" + posicion[0] + "][" + posicion[1] + "] con " + temperaturas[posicion[0]][posicion[1]] + " grados");
 
     }
     public static void llenarMatriz(int[][] m) {
@@ -49,9 +49,46 @@ public class TemperaturasSemanales {
         }
         return contador;
     }
-
-    
-
+    public static double promedioDiasFrescos(int[][] m) {
+        int suma = 0;
+        int cantidad = 0;
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                if (m[i][j] < 20) {
+                    suma += m[i][j];
+                    cantidad++;
+                }
+            }
+        }
+        if (cantidad == 0) {
+            return 0;
+        }
+        return (double) suma / cantidad;
+    }
+    public static void imprimirTemperaturasExtremas(int[][] m) {
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                if (m[i][j] > 35) {
+                    System.out.println("Temperatura crítica en [" + i + "][" + j + "]: " + m[i][j] + " grados");
+                }
+            }
+        }
+    }
+   public static int[] posicionTemperaturaMasAlta(int[][] m) {
+        int max = m[0][0];
+        int fila = 0;
+        int col = 0;
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                if (m[i][j] > max) {
+                    max = m[i][j];
+                    fila = i;
+                    col = j;
+                }
+            }
+        }
+        return new int[]{fila, col};
+    }
 }
 
     
